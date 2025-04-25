86a35aeae934617bd404d6447c58df0dca50ec95
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-04-25 14:20:07 -0300

    Gerando aplicação

---
+++ src/main/java/rede/social/nester/NesterApplication.java
@@ -0,0 +1,13 @@
+package rede.social.nester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rede/social/nester/NesterApplicationTests.java
@@ -0,0 +1,13 @@
+package rede.social.nester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
